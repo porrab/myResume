fc2f8fbc80d2b5d479db68461d430aac28d73f94
porrab <thanaphumsani@gmail.com>
2025-09-03 15:14:43 +0700

    update text

--- src/component/resume/Profile.tsx
+++ src/component/resume/Profile.tsx
@@ -4,12 +4,12 @@ const Profile = () => {
       <h2 className="text-2xl font-bold text-gray-800">PROFILE</h2>
       <hr className="my-2 border" />
       <p className="text-base text-gray-700 leading-relaxed">
-        I am a 3rd-year Information Technology student (KMUTT) interest in
-        Frontend Development. Most of my experience comes from hands-on projects
-        such as Life Connect and IT Bangmod Mobile Shop, demonstrating skills in
-        API integration and creating user-focused solutions. Seeking a Frontend
-        Developer internship to contribute to a dynamic team and grow within an
-        Agile environment.
+        I am a 3rd-year Information Technology student (KMUTT) passionate about
+        Frontend Development. My experience is built upon hands-on projects such
+        as 'Life Connect' and 'IT Bangmod Mobile Shop', which have provided me
+        with practical skills in front-end technologies and API integration.
+        Seeking a Frontend Developer internship to contribute to a dynamic team
+        and grow within an Agile environment.
       </p>
     </div>
   );
